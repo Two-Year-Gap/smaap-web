759060f5f4a90dd8c799fcf6df182d7ebf64a867
dekoms <dekoms@naver.com>
2024-10-25 20:12:07 +0900

    fix: 네이버 지도 script 의존성 제거
    index.tsx에서 script 동적 관리

--- src/components/NaverMap/NaverMap.tsx
+++ src/components/NaverMap/NaverMap.tsx
@@ -4,40 +4,23 @@ import './NaverMap.css';
 const NaverMap = () => {
   const mapRef = useRef<HTMLDivElement>(null);
 
+  // 스크립트 로드 후 지도 초기화
+
+  // 지도 초기화만 수행.
   useEffect(() => {
-    // 네이버 지도 스크립트 동적 추가
-    const loadNaverMapScript = () => {
-      const naverMapScript = document.createElement('script');
-      naverMapScript.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.REACT_APP_CLIENT_ID}`;
-      naverMapScript.defer = true;
+    if (!mapRef.current || !window.naver) return;
 
-      // 스크립트 로드 후 지도 초기화
-      naverMapScript.onload = initializeMap;
-      document.head.appendChild(naverMapScript); // 스크립트를 헤드에 추가
-    };
+    // 지도 생성
+    const map = new window.naver.maps.Map(mapRef.current, {
+      center: new window.naver.maps.LatLng(37.5665, 126.978),
+      zoom: 10,
+    });
 
-    const initializeMap = () => {
-      if (!mapRef.current || !window.naver) return;
-
-      // 지도 생성
-      const map = new window.naver.maps.Map(mapRef.current, {
-        center: new window.naver.maps.LatLng(37.5665, 126.978),
-        zoom: 10,
-      });
-
-      // 마커 추가
-      new window.naver.maps.Marker({
-        position: new window.naver.maps.LatLng(37.5665, 126.978),
-        map: map,
-      });
-    };
-
-    // 네이버 지도 스크립트 로드
-    if (!window.naver) {
-      loadNaverMapScript();
-    } else {
-      initializeMap();
-    }
+    // 마커 추가
+    new window.naver.maps.Marker({
+      position: new window.naver.maps.LatLng(37.5665, 126.978),
+      map: map,
+    });
   }, []);
 
   return <div className="map" ref={mapRef}></div>;
--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// 환경 변수를 사용해 네이버 지도 API 스크립트를 추가
-// const naverMapScript = document.createElement('script');
-// naverMapScript.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.REACT_APP_CLIENT_ID}&submodules=geocoder,drawing`;
-// naverMapScript.defer = true;
-// document.head.appendChild(naverMapScript);
+// 네이버 지도 스크립트 동적 추가
+const loadNaverMapScript = async () => {
+  return new Promise<void>((resolve) => {
+    const naverMapScript = document.createElement('script');
+    naverMapScript.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.REACT_APP_CLIENT_ID}&submodules=geocoder,drawing`;
+    naverMapScript.defer = true;
+    naverMapScript.onload = () => resolve(); // 스크립트 로드 완료 시 resolve 호출
+    document.head.appendChild(naverMapScript); // 스크립트를 헤드에 추가
+  });
+};
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+(async () => {
+  await loadNaverMapScript(); // 스크립트 로드 대기 후 App 렌더링
+
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement,
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+})();
